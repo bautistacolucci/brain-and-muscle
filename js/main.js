f503ccc71dcb8bbfb00ba13979a6f8631e0ad076
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-07-13 20:42:17 -0300

    footer y planes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ let divPlanes = document.createElement("div");
 
 btnPlanA.addEventListener("click", () => {
     detallePlanes.innerHTML = "";
-    divPlanes.innerHTML = `<h2 id="detallePlan">Hola</h2>`;
+    divPlanes.innerHTML = `<h3>Plan A</h3>
+    <p>Ideal para quien desea generar un cambio grande pero no sabe como hacerlo, nos encargamos de planificar la
+      alimentacion diaria, rutina, y realizamos calculo de grasa corporal mediante plicometria para lograr una mayor
+      precision en los resultados</p>
+    <div class="items">
+      <p>Dieta cerrada</p> <span></span>
+      <p>Mesociclo</p> <span></span>
+      <p>Plicometria</p>
+    </div>
+    <a href="contact.html" class="button"> Contactar </a>`;
     detallePlanes.appendChild(divPlanes);
 });
 btnPlanB.addEventListener("click", () => {
